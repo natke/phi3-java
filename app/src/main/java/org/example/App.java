5bed760bf0dfe4af53700e88d91d8d87a98509e7
Nat Kershaw <nakersha@microsoft.com>
2024-07-02 22:10:06 -0700

    Add calls to loop

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,22 +2,64 @@ package org.example;
 
 import java.util.logging.Logger;
 
-import ai.onnxruntime.genai.GenAIException;
-import ai.onnxruntime.genai.GeneratorParams;
-import ai.onnxruntime.genai.SimpleGenAI;
+import ai.onnxruntime.genai.*;
 
 public class App {
 
     private static final Logger logger = Logger.getLogger(App.class.getName());
 
-    public static void main(String[] args) throws GenAIException{
- 
-        SimpleGenAI generator = new SimpleGenAI("C:\\Users\\nakersha\\Develop\\models\\microsoft\\phi3-mini-4k\\cpu-int4-rtn-block-32-acc-level-4");
-        GeneratorParams params = generator.createGeneratorParams("<|user|>\\n" + //
-                        "Hello!<|end|>\\n" + //
-                        "<|assistant|>");
+    private void runSimple() {
 
-        String result = generator.generate(params, null);
-        logger.info("Result: " + result);
+        try {
+            SimpleGenAI generator = new SimpleGenAI("C:\\Users\\nakersha\\Develop\\models\\microsoft\\phi3-mini-4k\\cpu-int4-rtn-block-32-acc-level-4");
+            GeneratorParams params = generator.createGeneratorParams("<|user|>\\nHello!<|end|>\\n<|assistant|>");
+
+            String result = generator.generate(params, null);
+            logger.info("Result: " + result);
+        } catch (GenAIException e) {
+            e.printStackTrace();    
+
+        }
+    }
+
+    private void runLoop() {
+       Model model;
+       Tokenizer tokenizer;
+       TokenizerStream stream;
+
+        try {
+           model = new Model("C:\\Users\\nakersha\\Develop\\models\\microsoft\\phi3-mini-4k\\cpu-int4-rtn-block-32-acc-level-4");
+           tokenizer = model.createTokenizer();
+           stream = tokenizer.createStream();
+
+           GeneratorParams params = model.createGeneratorParams();
+           params.setSearchOption("max_length", 200);
+
+           Generator generator = new Generator(model, params);
+
+           Sequences input_ids = tokenizer.encode("<|user|>\\nHello!<|end|>\\n<|assistant|>");
+           params.setInput(input_ids);
+           while (!generator.isDone()) {
+               generator.computeLogits();
+               generator.generateNextToken();
+
+               int token = generator.getLastTokenInSequence(0);
+
+               String string = stream.decode(token);
+
+               System.out.println(string);
+           }
+        
+        } catch (GenAIException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws GenAIException{
+        App app = new App();
+        app.runSimple();
+        app.runLoop();
+ 
     }
 }
